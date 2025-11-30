f724a99b0b8d6513c2da4de5cd8d64592f4fedb0
Vojtěch Večeřa <vojtech.vecera@previo.cz>
2025-11-30 13:02:58 +0100

    feat(gitlab): add Commit type for API response

--- internal/gitlab/types.go
+++ internal/gitlab/types.go
@@ -86,6 +86,12 @@ type NoteData struct {
 	Body         string `json:"body"`
 }
 
+type Commit struct {
+	ID      string `json:"id"`
+	Title   string `json:"title"`
+	Message string `json:"message"`
+}
+
 type Project struct {
 	ID                int    `json:"id"`
 	Name              string `json:"name"`
